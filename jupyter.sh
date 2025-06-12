1042f4b76746df7af3361d1caf813462c9e79f52
murakami <murakami@bitstar.jp>
2025-06-12 17:50:26 +0900

    JupyterLabのポート番号変更（LLM/RAG用とぶつかるため）

--- jupyter.sh
+++ jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./.venv/bin/activate
 
-jupyter-lab --port 8888 --ip 0.0.0.0 --NotebookApp.token=Abcdabcd12341234 --notebook-dir=./notebooks/
+jupyter-lab --port 8000 --ip 0.0.0.0 --NotebookApp.token=Abcdabcd12341234 --notebook-dir=./notebooks/
