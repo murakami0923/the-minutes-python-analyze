0ee35fec21e640fe618d6e5a27b654101e3b0baa
murakami <murakami@bitstar.jp>
2026-01-20 16:04:02 +0900

    JupyterLabのURLを表示

--- jupyter.sh
+++ jupyter.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 source ./.venv/bin/activate
 
+echo ""
+echo ""
+echo "http://localhost:8000/lab?token=Abcdabcd12341234"
+echo ""
+echo ""
+
 jupyter-lab --port 8000 --ip 0.0.0.0 --NotebookApp.token=Abcdabcd12341234 --notebook-dir=./notebooks/
